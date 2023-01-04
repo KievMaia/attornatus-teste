206bb8f93bd46426fa88c1092f4dc4d5a33e7698
KievMaia <kievmaia@gmail.com>
2023-01-04 13:52:03 -0300

    Implementado testes de integração

--- src/test/java/com/attornatus/teste/CadastroPessoaTest.java
+++ src/test/java/com/attornatus/teste/CadastroPessoaTest.java
@@ -3,18 +3,17 @@ package com.attornatus.teste;
 import static io.restassured.RestAssured.given;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestPropertySource("/application-test.properties")
 class CadastroPessoaTest {
 
 	@LocalServerPort
@@ -43,14 +42,58 @@ class CadastroPessoaTest {
 	public void deveRetornarStatus200_QuandoAtualizarPessoa() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		given()
-			.pathParam("pessoaId", 1L)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
 			.body("{ \"nome\": \"João\", \"dataNascimento\": \"1981-12-13\"}")
+		.when()
+			.put("/1")
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	} 
+	
+	@Test
+	public void deveRetornarStatus200_QuandoBuscaPessoa() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get("/1")
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	} 
+	
+	@Test
+	public void deveRetornarStatus200_QuandoListarPessoas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	} 
+	
+	@Test
+	public void deveRetornarStatus200_QuandoListarEnderecoPessoas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get("1/endereco")
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	} 
+	
+	@Test
+	public void deveRetornarStatus204_QuandoAdicionarEnderecoPrincipalPessoa() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		given()
 			.contentType(ContentType.JSON)
 			.accept(ContentType.JSON)
 		.when()
-			.put()
+			.put("1/endereco/1")
 		.then()
-			.statusCode(HttpStatus.OK.value());
+			.statusCode(HttpStatus.NO_CONTENT.value());
 	} 
 
 }
